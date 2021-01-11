f60a7bf4e2cc9bebb6232e88da218ab1dfee5083
raejun92 <cvcv1324@naver.com>
2021-01-11 13:19:15 +0900

    Implement putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: juchoi <juchoi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/10 16:03:39 by juchoi            #+#    #+#             */
-/*   Updated: 2021/01/10 23:06:51 by juchoi           ###   ########.fr       */
+/*   Updated: 2021/01/11 13:18:45 by juchoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ void	ft_putnbr_fd(int n, int fd)
         ln = -1 * (long long)n;
         write(fd, "-", 1);
     }
-    ln = n;
     if (ln >= 10)
         ft_putnbr_fd((ln / 10), fd);
     ft_putchar_fd((ln % 10) + '0', fd);
