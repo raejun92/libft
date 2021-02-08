c49eaaabdbe27c89d685ac2de1717519a025a79f
Junho Choi <juchoi@c2r10s2.42seoul.kr>
2021-02-08 23:11:21 +0900

    Fix substr. Exception proccessing len of substr. if len > ft_strlen(s + start)     len = ft_strlen(s + start)

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juchoi <juchoi@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: juchoi <juchoi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 16:06:36 by juchoi            #+#    #+#             */
-/*   Updated: 2021/01/21 16:06:37 by juchoi           ###   ########.fr       */
+/*   Updated: 2021/02/08 23:09:15 by juchoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char				*str;
-	unsigned int		s_l;
+	unsigned int		s_len;
+	size_t				n_len;
 
 	if (!s)
 		return (0);
-	s_l = ft_strlen(s);
-	if (s_l < start)
+	s_len = ft_strlen(s);
+	if (s_len < start)
 		return (ft_strdup(""));
+	n_len = ft_strlen(s + start);
+	if (n_len < len)
+		len = n_len;
 	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
 		return (0);
 	ft_strlcpy(str, s + start, len + 1);
